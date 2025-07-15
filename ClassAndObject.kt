aa49353bfc1c0a81c7916c23273f411e0c84a710
BhraguTripathi <bhragu.12324742@lpu.in>
2025-07-16 02:05:27 +0530

    Add default constructor and input for Employee
    Updated the Employee class to provide default values for all properties, enabling instantiation without arguments. Added code in main to create an Employee object using user input for its properties and display the details.

--- ClassAndObject.kt
+++ ClassAndObject.kt
@@ -1,4 +1,9 @@
-class Employee(val name: String, val age: Int, val gender: Char, val salary: Double){
+class Employee(
+    var name: String=" ",
+    var age: Int=0,
+    var gender: Char=' ',
+    var salary: Double=0.0
+){
     fun showDetails(){
         println("Name: $name")
         println("Age: $age")
@@ -9,4 +14,13 @@ class Employee(val name: String, val age: Int, val gender: Char, val salary: Dou
 fun main(){
     val employee1=Employee("Bhragu",22,'M',220000.00)
     employee1.showDetails()
+
+    var emp2=Employee()
+    emp2.name=readLine()!!
+    emp2.age=readLine()!!.toInt()
+    emp2.gender=readLine()!!.first()
+    emp2.salary=readLine()!!.toDouble()
+    emp2.showDetails()
+
+
 }
